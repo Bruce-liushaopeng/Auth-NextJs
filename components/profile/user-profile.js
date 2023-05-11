05b6a3df4fe9d05049dc52aabe27d385b8f46e01
bruceliu <bruceliu1107@gmail.com>
2023-05-11 18:34:57 -0400

    server side check session to avoid front-end flashing

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -7,24 +7,24 @@ function UserProfile() {
   // Redirect away if NOT auth
   // for useSession, even page is loaded, sometimes the loading is still stay true.
   //const [session, loading] = useSession();
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
   
-  // getSession send a new request to get the latest loading and session, a way to work around that issue
-  useEffect(() => {
-    getSession().then(session => {
+  // // getSession send a new request to get the latest loading and session, a way to work around that issue
+  // useEffect(() => {
+  //   getSession().then(session => {
       
-      if(!session) {
-        window.location.href = '/auth' // if we don't find a session, we nevigate away
-      } else {
-        setIsLoading(false);
-      }
-    })
-  }, [])
-  console.log(isLoading)
+  //     if(!session) {
+  //       window.location.href = '/auth' // if we don't find a session, we nevigate away
+  //     } else {
+  //       setIsLoading(false);
+  //     }
+  //   })
+  // }, [])
+  // console.log(isLoading)
 
-  if (isLoading) {
-    return <p className={classes.profile }> Loading... </p>
-  }
+  // if (isLoading) {
+  //   return <p className={classes.profile }> Loading... </p>
+  // }
 
   return (
     <section className={classes.profile}>
--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,27 @@
-import UserProfile from '../components/profile/user-profile';
+import UserProfile from "../components/profile/user-profile";
+import { getSession } from "next-auth/client";
 
 function ProfilePage() {
   return <UserProfile />;
+}
+
+// we don't use getStaticProps, because getStaticProps mainly works in the build time, but in this case every request's context does matter
+export async function getServerSideProps(context) {
+  // look into the request and getSession and extract the cokie and see if it's valid
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false, // we want to only re-direct this time, becase the user is not loged in, but not permanently
+      }, // this way we set the redirect, the front end side won't even flashing
+    };
+  }
+
+  return {
+    props: { session },
+  };
 }
 
 export default ProfilePage;
